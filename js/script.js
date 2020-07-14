099d5e6fe30da250f6e153b3d41d5d1dd10b4a54
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-14 13:55:15 +0200

    ok

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// *****FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.****
+
+// 1 Per prima cosa bisogna creare un loop che permetta di stampare i numeri da 1 a 100.
+// 2 devo sostituire i multipli di 3, 5, e di entrambi ( quindi 3*5=15) con Fizz , Buzz e FizzBuzz utilizzando il resto.
+// (es. se il resto di "i" / 3 è uguale a 0 allora è un multiplo di tre).
+// 3 mettere prima i multipli di altrimenti sono vere anche le altre due condizioni.
+// 4 mostrarli con "console.log()"";
+// 5 mettere il "console.log();" nell'else alla fine. Se messo prima dell' "if" stampa tutti i numer compresi i multipli di 3 e 5;
+
+
+for (var i = 1; i <=100 ; i++) {
+
+    if (i % 15 == 0) {
+        console.log("FizzBuzz");
+    }else if (i % 3 == 0) {
+        console.log("Fizz");
+    }else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
